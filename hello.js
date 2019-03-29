a18c8819697c60f535abec8dbec11b33a7609522
Nhu Dinh Toan <toanbk21096@gmail.com>
2019-03-29 10:56:24 +0700

    update port

--- hello.js
+++ hello.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Travis!\n');
-}).listen(1337, 'localhost');
+}).listen(5000, 'localhost');
 console.log('Server running at http://localhost:1337/');
