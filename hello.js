62e120ecd0dec6abde7a5898d9e3da63b2233df2
Nhu Dinh Toan <toanbk21096@gmail.com>
2019-03-29 11:10:49 +0700

    fix port

--- hello.js
+++ hello.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Travis!\n');
-}).listen(6000, 'localhost');
-console.log('Server running at http://localhost:6000/');
+}).listen(4000, 'localhost');
+console.log('Server running at http://localhost:4000/');
