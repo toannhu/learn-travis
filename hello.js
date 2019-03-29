6dd69a26b00733d10ffb6eba667791c33d194d21
Nhu Dinh Toan <toanbk21096@gmail.com>
2019-03-29 11:35:04 +0700

    update project

--- hello.js
+++ hello.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Travis!\n');
-}).listen(4000, 'localhost');
-console.log('Server running at http://localhost:4000/');
+}).listen(4000);
+console.log(new Date().toString() + ' Server running at http://localhost:4000/');
